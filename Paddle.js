535cf3fb97f30e0d39c1155c6714248019f4642e
Brandon <rbrandon087@gmail.com>
2023-04-01 16:34:12 -0700

    added the speed for the paddles

--- Paddle.js
+++ Paddle.js
@@ -1,25 +1,30 @@
-const SPEED = .02
+const SPEED = 0.02
 
-export default class Paddle     {
-    constructor(paddleElem) {
-        this.paddleElem = paddleElem;
-    }
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem
+    this.reset()
+  }
 
-    get position() {
-        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue('--position'));
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    )
+  }
 
-    }
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value)
+  }
 
-    set position(value) {
-        this.ballElem.style.setProperty("--position", value)
-    }
+  rect() {
+    return this.paddleElem.getBoundingClientRect()
+  }
 
-    reset() {
-        this.position = 50;
-    }
-    update(delta, ballHeight) {
-        this.position += SPEED * delta * (ballHeight - this.position)
-    }
+  reset() {
+    this.position = 50
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position)
+  }
 }
-
-
