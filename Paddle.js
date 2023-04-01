c3b3f9a1372aa3833504cbc0e6e46eef1753ac28
Brandon <rbrandon087@gmail.com>
2023-04-01 16:24:57 -0700

    set up a paddle file to have my paddle move around while you go after the ball

---
+++ Paddle.js
@@ -0,0 +1,25 @@
+const SPEED = .02
+
+export default class Paddle     {
+    constructor(paddleElem) {
+        this.paddleElem = paddleElem;
+    }
+
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue('--position'));
+
+    }
+
+    set position(value) {
+        this.ballElem.style.setProperty("--position", value)
+    }
+
+    reset() {
+        this.position = 50;
+    }
+    update(delta, ballHeight) {
+        this.position += SPEED * delta * (ballHeight - this.position)
+    }
+}
+
+
--- script.js
+++ script.js
@@ -2,12 +2,19 @@ import Ball from "./Ball"
 import Paddle from "./Paddle.js"
 
 const ball = new Ball(document.getElementById('ball'))
+const playerPaddle = new Paddle(document.getElementById('player-Paddle'))
+const computerPaddle = new Paddle(document.getElementById('computer-Paddle'))
 
 let lastTime
 function update(time) {
-    IF (lastTime != null) {
+    if (lastTime != null) {
         const delta = time - lastTime;
         ball.update(delta);
+        computerPaddle.update(delta, ball.y);
+
+        if (isLose()) {
+            
+        }
     }
 
     lastTime = time;
@@ -15,5 +22,9 @@ function update(time) {
     window.requestAnimationFrame(update)
 }
 
+
+document.addEventListener("mousemove" , e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100;
+})
 
 window.requestAnimationFrame(update)
