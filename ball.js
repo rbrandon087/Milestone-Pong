cbbf3378852873e90a23370a22c128b756c1cc6e
Brandon <rbrandon087@gmail.com>
2023-04-01 16:01:59 -0700

    added my js code to my Ball file allowing my ball to move around

--- ball.js
+++ ball.js
@@ -1,5 +1,48 @@
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
+
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem;
+        this.reset();
     }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue(--x))
+    }
+
+    set x(value) {
+        this.ballElem.style.setProperty("--y". value)
+    }
+
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue(--y))
+    }
+
+    set y(value) {
+        this.ballElem.style.setProperty("--y". value)
+    }
+
+    reset () {
+        this.x = 50;
+        this.y = 50;
+        this.direction = { x: 0 }
+        while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >=.9) {
+            const heading = randomNumberBetween(0, 2 * Math.PI)
+            this.direction = { x: Math.cos(heading), y:Math.sin(heading) }
+        }
+        this.velocity = INITIAL_VELOCITY
+    }
+
+    update(delta) {
+        this.x += this.direction.x * this.velocity * delta
+        this.y += this.direction.x * this.velocity * delta
+
+    }
+}
+
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min;
 }
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-import ball from './ball.js'
+import Ball from "./Ball"
+import Paddle from "./Paddle.js"
 
 const ball = new Ball(document.getElementById('ball'))
+
+let lastTime
+function update(time) {
+    IF (lastTime != null) {
+        const delta = time - lastTime;
+        ball.update(delta);
+    }
+
+    lastTime = time;
+    
+    window.requestAnimationFrame(update)
+}
+
+
+window.requestAnimationFrame(update)
